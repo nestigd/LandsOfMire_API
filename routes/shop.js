830077ed74c0be4045ed3b992d0769be1063eb0c
unknown <nestigd95@live.com>
2022-12-26 10:13:22 +0100

    started integration with frontend

--- routes/shop.js
+++ routes/shop.js
@@ -6,4 +6,28 @@ router.get("/", (req, res) => {
   res.json({ mssg: "test successful" });
 });
 
+router.get("/onsale", (req, res) => {
+  // TODO: Items on sale need to be taken from the database
+  res.json({
+    item1: {
+      title: "Iron sword",
+      fullPrice: 12,
+      price: 8,
+      toExpiry: 5,
+    },
+    item2: {
+      title: "Black steel hammer",
+      fullPrice: 24,
+      price: 16,
+      toExpiry: 1,
+    },
+    item3: {
+      title: "Black steel hammer",
+      fullPrice: 24,
+      price: 16,
+      toExpiry: 1,
+    },
+  });
+});
+
 module.exports = router;
