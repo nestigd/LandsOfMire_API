6b54aa7c2811873a9fc060f1108c5394b6451c1b
nestigd <nestigd95@live.com>
2022-12-30 14:26:24 +0100

    implemented authentication on server side

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,8 @@
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
-const loginUser = async (req,res) => {
-    res.json(
-        {mssg:"login user"}
-    )
+const createToken = (_id)=>{
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn:"2d"})
 }
 
 const signupUser = async (req,res) => {
@@ -11,11 +10,26 @@ const signupUser = async (req,res) => {
 
     try {
         const user = await User.signup(email,password);
+        const token = createToken(user._id)
 
-        res.status(200).json({email, user})
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error:error.message})
     }
+}
+
+const loginUser = async (req,res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error:error.message})
+    }
+
 }
 
 module.exports = { signupUser, loginUser}
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -18,6 +19,17 @@ const userSchema = new Schema({
 // static method: signup(hash passwords before saving new user)
 // remember to use regular function for proper "this" behavior
 userSchema.statics.signup = async function (email,password){
+    
+    //validation
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Not a valid email address')
+    }if(!validator.isStrongPassword(password)){
+        throw Error('The password must contain at least: 8 characters, 1 lower case, 1 upper case, 1 number, 1 letter and 1 special character')
+    }
+
     const exists = await this.findOne({email});
 
     if (exists){
@@ -25,11 +37,35 @@ userSchema.statics.signup = async function (email,password){
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password,salt);
-
+    const hash = await bcrypt.hash(password, salt);
     const user = await this.create({email, password:hash})
 
     return user
+}
+
+userSchema.statics.login = async function (email,password){
+
+    console.log(email + " " + password);
+
+    if (!email || !password){
+        throw Error('All fields must be filled');
+    };
+
+    
+    const user = await this.findOne({email});
+    
+    if (!user){
+        throw Error('email not valid');
+    };
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match){
+        throw Error('password not valid');
+    };
+
+    return user
+
 }
 
 module.exports = mongoose.model('User', userSchema)
